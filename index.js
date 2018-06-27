93cae6cdc704eb4c965918999e61ae7464e45753
nagits <aleksey.nagits@gmail.com>
2018-06-28 03:16:29 +0500

    [WIP] RC

--- index.js
+++ index.js
@@ -1,83 +1,67 @@
 'use strict';
 
-var Funnel = require('broccoli-funnel');
-var Merge = require('broccoli-merge-trees');
-var concat = require('broccoli-concat');
-//var Plugin = require('broccoli-plugin');
-var Plugin = require('broccoli-caching-writer');
-var path = require('path');
-var fs = require('fs');
-var glob = require('glob');
-var Atomizer = require('atomizer');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
 
-// Create a subclass MyCompiler derived from Plugin
-MyCompiler.prototype = Object.create(Plugin.prototype);
-MyCompiler.prototype.constructor = MyCompiler;
-function MyCompiler(inputNodes, options) {
-  options = options || {};
-  Plugin.call(this, inputNodes, {
-    annotation: options.annotation
-  });
-  this.options = options;
+const BroccoliPlugin = require('broccoli-caching-writer');
+const MergeTrees = require('broccoli-merge-trees');
+
+const Atomizer = require('atomizer');
+
+const CSS_FILE_NAME = 'atomic.css';
+
+AtomizerCompiler.prototype = Object.create(BroccoliPlugin.prototype);
+AtomizerCompiler.prototype.constructor = AtomizerCompiler;
+function AtomizerCompiler(inputNodes, options) {
+  BroccoliPlugin.call(this, inputNodes);
+  this.options = options || {};
 }
 
-MyCompiler.prototype.build = function() {
-  let templatesStr = this.inputPaths.map(path => {
-    let inputBuffer = glob.sync(path + '**/*.hbs').map(file => {
+AtomizerCompiler.prototype.build = function() {
+  let pattern = this.options.pattern || '**/*.hbs';
+  let templatesStr = this.inputPaths.map(inputPath => {
+    return glob.sync(path.join(inputPath, pattern)).map(file => {
       return fs.readFileSync(file);
     }).join('\n');
-
-    return inputBuffer;
   }).join('\n');
 
-  var atomizer = new Atomizer({verbose: true});
-  var foundClasses = atomizer.findClassNames(templatesStr);
-  var finalConfig = atomizer.getConfig(foundClasses, {});
-  var css = atomizer.getCss(finalConfig);
+  let atomizer = new Atomizer({verbose: true});
+  let foundClasses = atomizer.findClassNames(templatesStr);
 
-  fs.writeFileSync(path.join(this.outputPath, 'atomic.css'), css);
+  // Make shallow copy to avoid mutation of `options.config` in
+  // `atomizer.getConfig` func: https://git.io/f4dyl.
+  // Otherwise, the result of the second compilation will be incorrect.
+  let defaultConfig = { ... this.options.config };
+  let config = atomizer.getConfig(foundClasses, defaultConfig);
+  let css = atomizer.getCss(config);
+
+  let cssOutputPath = path.join(this.outputPath, CSS_FILE_NAME);
+  fs.writeFileSync(cssOutputPath, css);
 };
 
 module.exports = {
   name: 'ember-cli-atomizer',
 
   treeForVendor(tree) {
-    if (!this._isAddon()) {
-      let templatesPath = path.join(this.projectRoot, 'templates');
-      let comp = new MyCompiler([templatesPath], {});
+    let host = this._findHost();
+    let options = host.options[this.name] || {};
+    let templatesPaths = options.dir
+        ? Array.isArray(options.dir) ? options.dir : [options.dir]
+        : [host.trees.templates];
 
-      let trees = [tree, comp].filter(Boolean);
+    let compiler = new AtomizerCompiler(templatesPaths, options);
+    let trees = [tree, compiler].filter(Boolean);
 
-      tree = new Merge(trees, {
-        annotation: 'asdasdsad1243'
-      });
-    }
+    tree = new MergeTrees(trees, {
+      annotation: this.name
+    });
 
     return this._super.treeForVendor.call(this, tree);
   },
 
-  included: function(app) {
-    this.projectRoot = this._projectRoot(app.trees);
-
-    app.import('vendor/atomic.css');
-
+  included(parent) {
+    this.import('vendor/' + CSS_FILE_NAME);
     return this._super.included.apply(this, arguments);
-  },
-
-  _isAddon: function() {
-    return Boolean(this.parent.parent);
-  },
-
-  _projectRoot: function(trees) {
-    var projectRoot;
-    if (this._isAddon()) {
-      projectRoot = this.parent.root + '/addon';
-    } else if (trees && trees.app) {
-      projectRoot = trees.app;
-    } else {
-      projectRoot = this.parent.root + '/app';
-    }
-
-    return projectRoot;
   }
 };
